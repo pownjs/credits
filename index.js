af4dc713baa5b8ca438e8633aa63e319568e11c4
Petko D. Petkov <pdp@websecurify.com>
2017-03-16 22:55:56 +0000

    Improved banner

--- index.js
+++ index.js
@@ -12,25 +12,32 @@ exports.yargs = {
 
     handler: (argv) => {
         const chalk = require('chalk')
+        const wordwrap = require('wordwrap')
         const pownModules = require('pown-modules')
 
         console.log(chalk.yellow(`
-88888b.  8888b. 888  888  88888888b.
-888 "88b    "88b888  888  888888 "88b
-888  888.d888888888  888  888888  888
-888 d88P888  888Y88b 888 d88P888  888
-88888P" "Y888888 "Y8888888P" 888  888
-888    d8b
-888    Y8P
-888
-      8888 .d8888b
-      "888 88K
-       888 "Y8888b.
-       888      X88
-       888  88888P'
-       888
-      d88P
-    888P"
++---------------------------------------------+
+|                                             |
+|                                             |
+|    88888b.  8888b. 888  888  88888888b.     |
+|    888 "88b    "88b888  888  888888 "88b    |
+|    888  888.d888888888  888  888888  888    |
+|    888 d88P888  888Y88b 888 d88P888  888    |
+|    88888P" "Y888888 "Y8888888P" 888  888    |
+|    888    d8b                               |
+|    888    Y8P                               |
+|    888                                      |
+|          8888 .d8888b                       |
+|          "888 88K                           |
+|           888 "Y8888b.                      |
+|           888      X88                      |
+|           888  88888P'                      |
+|           888                               |
+|          d88P                               |
+|        888P"                                |
+|                                             |
+|                                             |
++---------------------------------------------+
 `))
 
         let list
@@ -60,8 +67,10 @@ exports.yargs = {
                 }
             })
 
+            const wrap = wordwrap(79)
+
             if (people.length) {
-                console.log(`\nContributors:\n ${Array.from(new Set(people)).join(', ')}\n`)
+                console.log(wrap(`\n${chalk.magenta('Pown.js is possible because of the following people:')}\n\n${Array.from(new Set(people)).join(', ')}\n`))
             }
         })
     } 
